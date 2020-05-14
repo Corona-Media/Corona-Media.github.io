a186c54de9a198caa252168094df4ccd622455df
IlanLuci <53833154+IlanLuci@users.noreply.github.com>
2020-05-14 09:52:06 -0400

    Add Feedback Form

---
+++ Hosted On Other Site/feedback.php
@@ -0,0 +1,9 @@
+<?php
+
+$myfile = fopen("feedback.txt", "a") or die("Unable to open file!");
+$txt = $_POST["message"] . "\n";
+fwrite($myfile, $txt);
+fclose($myfile);
+
+header("Location: https://corona-media.github.io/feedback.html");
+?>
