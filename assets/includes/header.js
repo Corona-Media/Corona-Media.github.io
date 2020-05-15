d0c95727eacd4073bfa779064e8ed74eaedede18
IlanLuci <53833154+IlanLuci@users.noreply.github.com>
2020-05-15 15:22:45 -0400

    Beta Release

---
+++ assets/includes/header.js
@@ -0,0 +1,10 @@
+ document.write("<header class='bg-dark text-center pt-1' style='position:fixed; top:0; width: 100%; z-index: 1;'>");
+ document.write("<h1><a href='index.html' class='title'>Covid Kids</a></h1>");
+ document.write("<h1 class='title1' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>&#9776;</h1>");
+ document.write("<div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>");
+ document.write("<a class='dropdown-item' href='index.html'>Home</a>");
+ document.write("<a class='dropdown-item' href='videos.html'>Videos</a>");
+ document.write("<a class='dropdown-item' href='news.html'>News</a>");
+ document.write("<a class='dropdown-item' href='feedback.html'>Feedback</a>");
+ document.write("</div>");
+ document.write("</header>");
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+//start function 
+function start() {
+    console.clear();
+    console.log("This is a work in progress. Please suggest any improvements on the feedback page https://corona-media.github.io/feedback.html");
+}
+
+//video code
 var videos = ["https://www.youtube.com/embed/ykieEE1j9eA", "https://youtube.com/embed/M5azNpTwVk8", "https://www.youtube.com/embed/e0-2XxgHIXk", "https://youtube.com/embed/xWdPusIK8XU", "https://www.youtube.com/embed/arMKDDSwrTo", "https://youtube.com/embed/xGKFVMgjrPc", "https://www.youtube.com/embed/cqvVL8IurMw"];
         
 for (var x = 0; x < videos.length; x += 1) {
@@ -9,4 +16,46 @@ for (var x = 0; x < videos.length; x += 1) {
     newContent.style = "width:  500px;";
     newContent.innerHTML = div;
     addTo.appendChild(newContent);
+}
+
+//Ideas gen code
+function generate() {
+
+//Create array of ideas and links
+var words = [{name: "Read", link: null},
+             {name: "Play Video Games", link: 'https://www.epicgames.com/store'},
+             {name: "Call Or Text A Friend", link: null},
+             {name: "Listen To Music", link: 'https://www.spotify.com/us/'},
+             {name: "Learn A Musical Instrument", link: 'https://www.udemy.com/topic/piano/'},
+             {name: "Watch Youtube", link: 'https://www.youtube.com/'},
+             {name: "Watch A Movie Or TV Show", link: 'https://www.netflix.com/'},
+             {name: "Play A Board Or Card Game", link: 'https://www.gearhungry.com/best-family-board-games/'},
+             {name: "Read The News", link: 'https://news.google.com/'},
+             {name: "Learn A New Language", link: 'https://www.duolingo.com/'},
+             {name: "Do Extra Chores", link: null},
+             {name: "Learn To Code", link: 'https://www.khanacademy.org/computing/computer-programming'},
+             {name: "Take an Online Course", link: 'https://www.edx.org'},
+             {name: "Write A Story", link: null},
+             {name: "Write A Journal Or Diary", link: 'https://myjournalate.com/'},
+             {name: "Build A Pillow Fort", link: null},
+             {name: "Build A Cardboard Box Fort", link: 'https://handsonaswegrow.com/build-fun-cardboard-fort/'},
+             {name: "Go On A Bike Ride, Run, Or Walk", link: null},
+             ];
+
+//Set link text to "Learn More"
+document.getElementById("link").innerHTML = "I want to try this.";
+
+//Set text to random idea and set link to correct url
+var number = Math.floor(Math.random() * words.length);
+
+var word = words[number].name;
+document.getElementById("text").innerHTML = word;
+
+var link = words[number].link;
+    if (link != null) {
+        document.getElementById("link").href = link;
+    } else {
+        document.getElementById("link").removeAttribute("href");
+        document.getElementById("link").innerHTML = "";
+    }
 }
