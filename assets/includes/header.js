acdf98c18575a30d719074ae8ab429a0fbee0276
IlanLuci <53833154+IlanLuci@users.noreply.github.com>
2020-05-22 21:16:24 -0400

    Styling Fixes

--- assets/includes/header.js
+++ assets/includes/header.js
@@ -1,5 +1,5 @@
- document.write("<header class='bg-dark text-center pt-1' style='position:fixed; top:0; width: 100%; z-index: 1;'>");
- document.write("<h1 class='maintitle'><a href='index.html' class='title'>Covid Kids</a></h1>");
+ document.write("<header class='bg-dark text-center pt-2' style='position:fixed; top:0; width: 100%; z-index: 1;'>");
+ document.write("<h1><a href='index.html' class='title'>Covid Kids</a></h1>");
  document.write("<h1 class='title1 unselectable' id='dropdownMenuButton' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>&#9776;</h1>");
  document.write("<div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>");
  document.write("<a class='dropdown-item' href='index.html'>Home</a>");
