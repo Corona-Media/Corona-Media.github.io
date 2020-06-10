797a3dc3ec570ed385bcbe1808626b5dcbe7016c
Ilan <therollbotgroup@gmail.com>
2020-06-10 17:13:17 -0400

    Content additions

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,8 @@ var articles = [{image: "assets/images/dc.jpeg", description: "Hawthorne Summer 
                 {image: "assets/images/book-list.jpg", description: "20+ Great Books For Kids To Read While Stuck At Home", link: "assets/articles/book-list.html"},
                 {image: "assets/images/puzzle.jpg", description: "30+ Activities to keep you busy while bored at home", link: "assets/articles/activities-list.html"},
                 {image: "assets/images/corona.png", description: "A Firsthand Account Of A Kid Living Through The Pandemic", link: "assets/articles/kids-corona-experience.html"},
-                {image: "assets/images/camera.jpg", description: "25+ Great Family Friendly Movies To Pass The Time", link: "assets/articles/movie-list.html"}
+                {image: "assets/images/camera.jpg", description: "25+ Great Family Friendly Movies To Pass The Time", link: "assets/articles/movie-list.html"},
+                {image: "assets/images/summer-plan.png", description: "How To Plan Your Summer During These Uncertain Times", link: "assets/articles/summer-planning.html"}
                ];
 
 
