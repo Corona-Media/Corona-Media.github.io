92b8993bdbd33e38bfc8b8ea57a54e8e75771e2b
IlanLuci <53833154+IlanLuci@users.noreply.github.com>
2020-06-17 18:12:51 -0400

    Version 1.0.4
    Add serviceworker

---
+++ sw.js
@@ -0,0 +1,60 @@
+const cacheName = 'covid-kids-cache-v1';
+const resourcesToPrecache = [
+    '/',
+    '/assets/articles/activities-list.html',
+    '/assets/articles/book-list.html',
+    '/assets/articles/discover-camp.html',
+    '/assets/articles/kids-corona-experience.html',
+    '/assets/articles/movie-list.html',
+    '/assets/articles/online-course.html',
+    '/assets/articles/planning-summer.html',
+    'https://api.covid19api.com/world/total',
+    '/index.html',
+    '/videos.html',
+    '/news.html',
+    '/mobilegame.html',
+    '/feedback.html',
+    '/credits.html',
+    '/artwork.html',
+    '/articles.html',
+    '/404.html',
+    '/assets/artwork/0/0.jpg',
+    '/assets/artwork/0/1.jpg',
+    '/assets/artwork/0/2.jpg',
+    '/assets/artwork/0/3.jpg',
+    '/assets/artwork/0/4.jpg',
+    '/assets/artwork/0/5.jpg',
+    '/assets/images/book-list.jpg',
+    '/assets/images/camera.jpg',
+    '/assets/images/corona.png',
+    '/assets/images/course-site.png',
+    '/assets/images/dc.jpeg',
+    '/assets/images/puzzle.jpg',
+    '/assets/images/summer-plan.png',
+    '/assets/js/main.js',
+    '/assets/js/bootstrap.min.js',
+    '/assets/js/bootstrap.min.js.map',
+    '/assets/js/jquery.min.js',
+    '/assets/includes/head.js',
+    '/assets/includes/header.js',
+    '/assets/fonts/BebasNeue-Bold.ttf',
+    '/assets/css/styles.css'
+];
+
+self.addEventListener('install', event => {
+    console.log('Service Worker Install Event');
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => {
+                return cache.addAll(resourcesToPrecache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request)
+        .then(cachedResponce => {
+            return cachedResponce || fetch(event.request);
+        })
+    );
+});
