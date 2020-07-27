d7bcadbd304139da23a265e6b9190e7d2930f77f
Ilan <therollbotgroup@gmail.com>
2020-07-27 17:16:59 -0400

    change backend do different repo

--- Hosted On Other Site/feedback.php
+++
@@ -1,7 +0,0 @@
-<?php
-$myfile = fopen("feedback.txt", "a") or die("Unable to open file!");
-$txt = $_POST["message"] . "\n";
-fwrite($myfile, $txt);
-fclose($myfile);
-header("Location: https://covidkidsonline.com/feedback.html");
-?>
--- Hosted On Other Site/feedback_popup.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_POST != "") {
-    $myfile = fopen("feedback.txt", "a") or die("Unable to open file!");
-    $txt = $_POST["message"] . "\n";
-    fwrite($myfile, $txt);
-    fclose($myfile);
-    header("Location: https://covidkidsonline.com");
-}
-?>
